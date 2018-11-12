2c74ce8dbcb7e39be3df2066482acdad848f53cf
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-11-12 18:24:02 +0100

    Played with Flask variables to check their handling. Example shown in input_main_input.html and app.py

--- rs_helper/classes/RecommendationFacade.py
+++ rs_helper/classes/RecommendationFacade.py
@@ -5,7 +5,10 @@ from rs_helper.classes import Prediction
 
 class RecommendationFacade:
 
-    def __init__(self):
+    def __init__(self, path_to_file: str):
+        corpora = Corpora(path=path_to_file)
+
+    def run(self, lda: bool = False, key_ex: bool = False, doc2vec: bool = False, classification: bool = False):
         pass
 
     def __lda_pipeline(self):
@@ -14,4 +17,4 @@ class RecommendationFacade:
     def __merge_predictions(self, predictions: list) -> Prediction:
         if not isinstance(predictions[0], Prediction):
             raise ValueError("Parameter predictions must be of type List(Prediction)")
-        pass
+        return Prediction()
