a80b4cc5c7ee8a8100cfd71ec5bc79f1b7f9622f
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-11-12 13:25:39 +0100

    Added the base structure of the python package

---
+++ rs_helper/classes/__init__.py
@@ -0,0 +1,6 @@
+from rs_helper.classes.Corpora import Corpora
+from rs_helper.classes.Vectorizer import Vectorizer
+from rs_helper.classes.RecommendationFacade import RecommendationFacade
+from rs_helper.classes.Prediction import Prediction
+from rs_helper.classes.SystemEvaluation import SystemEvaluation
+from rs_helper.classes.Preprocessor import Preprocessor
