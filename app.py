bffaa2eeacea6404a18ccc976f22721e2d326948
MonteChristo46 <ddad5100d!>
2018-11-14 00:09:26 +0100

    Save input function added
    (cherry picked from commit 1ce6c85)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
+from helper_functions import save_txt
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=["GET", "POST"])
 def main():
@@ -10,13 +10,16 @@ def main():
         problem_short_desc = "" if "short_description" not in request.form else request.form['short_description']
         problem_long_desc = "" if "long_description" not in request.form else request.form['long_description']
 
+        save_txt(problem_title, problem_short_desc, problem_long_desc)
+
         return render_template("index.html", params={
             "title": problem_title,
             "short": problem_short_desc,
             "long": problem_long_desc
         })
+
     return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
