3aa342c7444208bd4350765fce74dfeb6e8abeb7
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-11-13 10:24:52 +0100

    Tested with Flask variables to check their handling. Example shown in input_main_input.html and app.py; Added placeholder for image div; Added abstract class Augmentator

---
+++ rs_helper/classes/Augmentator.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+
+class Augmentator(ABC):
+
+    def __init__(self):
+        pass
--- rs_helper/classes/RecommendationFacade.py
+++ rs_helper/classes/RecommendationFacade.py
@@ -1,6 +1,7 @@
 from rs_helper.classes import Corpora
 from rs_helper.classes import Vectorizer
 from rs_helper.classes import Prediction
+from rs_helper.classes import Preprocessor
 
 
 class RecommendationFacade:
@@ -9,9 +10,23 @@ class RecommendationFacade:
         corpora = Corpora(path=path_to_file)
 
     def run(self, lda: bool = False, key_ex: bool = False, doc2vec: bool = False, classification: bool = False):
-        pass
+        """
+        Function should manage the starting of pipelines according to input
+        :param lda: boolean to start or not start LDA pipeline
+        :param key_ex: boolean to start or not start Keyword Extraction pipeline
+        :param doc2vec: boolean to start or not start Doc2Vec pipeline
+        :param classification: boolean to start or not start ML Classification pipeline
+        :return: Object of type Prediction (merged prediction of all pipelines)
+        """
+        if lda:
+            self.__lda_pipeline()
+        if key_ex:
+            self.__key_ex_pipeline()
 
     def __lda_pipeline(self):
+        pass
+
+    def __key_ex_pipeline(self):
         pass
 
     def __merge_predictions(self, predictions: list) -> Prediction:
