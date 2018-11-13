eff008aabb2472be0395517447ce0ae85d188cd3
MonteChristo46 <Ddad5100d!>
2018-11-13 22:57:51 +0100

    Sklearn.metric.confusion_matrix used

--- rs_helper/classes/SystemEvaluation.py
+++ rs_helper/classes/SystemEvaluation.py
@@ -1,19 +1,31 @@
 from rs_helper.classes import Prediction
+from sklearn.metrics import confusion_matrix
+from typing import *
 
 
 class SystemEvaluation:
     """
     This class is to evaluate the overall system and it's predicting power
     """
+
     def __init__(self, predictions: list, labels: list):
         if not isinstance(predictions[0], Prediction):
             raise ValueError("The parameter predictions need to be of type Prediction")
         self.predictions = predictions
         self.labels = labels
-        self.positives = 0
-        self.negatives = 0
 
-    def calculate_positives_and_negatives(self):
+        self.tn = 0
+        self.fp = 0
+        self.fn = 0
+        self.tp = 0
+
+        self.positives = self.tn + self.tp
+        self.negatives = self.fp + self.fn
+
+        self.__init_metrics()
+
+    # TODO I would use sklearn confusion matrix and use.ravel to get all tn,tp,.. Easier for us, more Eval.
+    def calculate_positives_and_negatives(self) -> Tuple[int, int]:
         """
         This method counts all positive and negative predictions
         :return: tuple(int, int)
@@ -26,11 +38,25 @@ class SystemEvaluation:
         self.negatives = neg
         return pos, neg
 
-    def calculate_recall(self):
-        pass
+    def __init_metrics(self):
+        self.tn, self.fp, self.fn, self.tp = confusion_matrix(y_true=self.labels, y_pred=self.predictions).ravel()
 
-    def calculate_accurracy(self):
-        pass
+    def calculate_recall(self):
+        return self.tp / (self.tp + self.fn)
+
+    def calculate_true_negative_rate(self):
+        return self.tn / (self.tn + self.fp)
+
+    def calculate_precision(self):
+        return (self.tp / (self.tp + self.fp))
+
+    def calculate_accuracy(self):
+        return (self.tp + self.tn) / (self.positives + self.negatives)
+
+    def get_confusion_matrix(self, labels: List[str] = None):
+        if labels:
+            return confusion_matrix(y_true=self.labels, y_pred=self.predictions, labels=labels)
+        return confusion_matrix(y_true=self.labels, y_pred=self.predictions)
 
     def save_evaluation(self) -> bool:
         """
