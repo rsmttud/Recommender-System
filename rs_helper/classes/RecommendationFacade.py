a755321f8aac0163d39ba2f9fff0d4da7075a14e
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-11-14 15:28:04 +0100

    Changed Topic Class to perform correct merging of Topic with __add__

--- rs_helper/classes/RecommendationFacade.py
+++ rs_helper/classes/RecommendationFacade.py
@@ -1,27 +1,35 @@
 from rs_helper.classes import Corpora
 from rs_helper.classes import Vectorizer
 from rs_helper.classes import Prediction
+from rs_helper.classes import Preprocessor
 
-# TODO implement the facade
+
 class RecommendationFacade:
-    """
-    A facade which contains of single functions for each classification pipeline.
-    """
 
     def __init__(self, path_to_file: str):
-        self.corpora = Corpora(path=path_to_file)
+        corpora = Corpora(path=path_to_file)
 
     def run(self, lda: bool = False, key_ex: bool = False, doc2vec: bool = False, classification: bool = False):
-        pass
+        """
+        Function should manage the starting of pipelines according to input
+        :param lda: boolean to start or not start LDA pipeline
+        :param key_ex: boolean to start or not start Keyword Extraction pipeline
+        :param doc2vec: boolean to start or not start Doc2Vec pipeline
+        :param classification: boolean to start or not start ML Classification pipeline
+        :return: Object of type Prediction (merged prediction of all pipelines)
+        """
+        if lda:
+            self.__lda_pipeline()
+        if key_ex:
+            self.__key_ex_pipeline()
 
     def __lda_pipeline(self):
         pass
 
-    # TODO __merge_predictions() need to be implemented
-    """
+    def __key_ex_pipeline(self):
+        pass
+
     def __merge_predictions(self, predictions: list) -> Prediction:
         if not isinstance(predictions[0], Prediction):
             raise ValueError("Parameter predictions must be of type List(Prediction)")
-        pass
-        # return Prediction()
-    """
+        return Prediction(["dummy"], ["dummy"])
