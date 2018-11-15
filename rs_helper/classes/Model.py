cc712cf6d5025c0992a237502548df8cf1161a5d
MonteChristo46 <Ddad5100d!>
2018-11-15 19:43:25 +0100

    Documentation and type hinting added.

--- rs_helper/classes/Model.py
+++ rs_helper/classes/Model.py
@@ -15,13 +15,28 @@ class Model(ABC):
         self.model = None
 
     @abstractmethod
-    def initialize(self):
+    def initialize(self) -> None:
+        """
+        The method initialize the model and save it to the class variable model
+        :return:
+        """
         pass
 
     @abstractmethod
     def predict(self) -> Prediction:
+        """
+        Each model needs to be able to predict something
+        :return: prediction object
+        """
         pass
 
+    # TODO Documentation necessary
     @abstractmethod
-    def normalize_result(self, prediction: Prediction):
+    def normalize_result(self, prediction: Prediction) -> Prediction:
+        """
+        To
+        :param prediction: a prediction made by a model in form of an prediction object
+        :return: A prediction object with normalized results
+        """
         pass
+
--- rs_helper/classes/RecommendationFacade.py
+++ rs_helper/classes/RecommendationFacade.py
@@ -2,11 +2,14 @@ from rs_helper.classes import Corpora
 from rs_helper.classes import Vectorizer
 from rs_helper.classes import Prediction
 
-
+# TODO implement the facade
 class RecommendationFacade:
+    """
+    A facade which contains of single functions for each classification pipeline.
+    """
 
     def __init__(self, path_to_file: str):
-        corpora = Corpora(path=path_to_file)
+        self.corpora = Corpora(path=path_to_file)
 
     def run(self, lda: bool = False, key_ex: bool = False, doc2vec: bool = False, classification: bool = False):
         pass
@@ -14,7 +17,11 @@ class RecommendationFacade:
     def __lda_pipeline(self):
         pass
 
+    # TODO __merge_predictions() need to be implemented
+    """
     def __merge_predictions(self, predictions: list) -> Prediction:
         if not isinstance(predictions[0], Prediction):
             raise ValueError("Parameter predictions must be of type List(Prediction)")
-        return Prediction()
+        pass
+        # return Prediction()
+    """
