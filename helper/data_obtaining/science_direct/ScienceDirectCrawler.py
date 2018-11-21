2b138a43c7deee0b6e08510a0211d8c84ec43302
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-11-21 12:39:39 +0100

    Updated Crawler to only store relevant information from hugh server response JSON

--- helper/data_obtaining/science_direct/ScienceDirectCrawler.py
+++ helper/data_obtaining/science_direct/ScienceDirectCrawler.py
@@ -12,7 +12,7 @@ class ScienceDirectCrawler:
         """
         :param query: Query that should be searched in science direct (https://dev.elsevier.com/tips/ScienceDirectSearchTips.htm)
         :param out_dir: Directory where the resulting files should be stored
-        :param to_be_num: Amount of articles to get crawled (Needs to be x times 100.
+        :param to_be_num: Amount of articles to get crawled (Needs to be x times 100).
         """
         if to_be_num % 100 != 0:
             raise ValueError("Number of results must be 100*x")
@@ -27,7 +27,7 @@ class ScienceDirectCrawler:
         self._status_code = ""
         self.config = self.__load_config()
         self.to_be_num = to_be_num
-        self.results = []
+        self.results = dict()
         self.total_nums = 0
         self.num_res = 0
         self.out_dir = out_dir
@@ -35,50 +35,26 @@ class ScienceDirectCrawler:
     def run(self):
         """
         :return: void
-         General function to start crawling.
+        General handler of crawling.
         """
         result = self.__exec_request(self.url)
-        self.results = result['search-results']['entry']
-        self.num_res += len(self.results)
-        self.total_nums = int(result['search-results']['opensearch:totalResults'])
+        self.__save_relevants_in_results(result, total=True)
+        self.num_res += len(self.results["documents"])
+        self.total_nums = self.results["total_results"]
 
-        if len(self.results) != self.to_be_num:
+        if len(self.results["documents"]) != self.to_be_num:
             while self.num_res < self.total_nums:
                 for el in result['search-results']['link']:
                     if el['@ref'] == 'next':
                         next_url = el['@href']
                         next_response = self.__exec_request(next_url)
-                        self.results += next_response['search-results']['entry']
-                        print(len(self.results))
-                        if len(self.results) == self.to_be_num:
+                        self.__save_relevants_in_results(next_response)
+                        print(len(self.results["documents"]))
+                        if len(self.results["documents"]) == self.to_be_num:
                             break
-                if len(self.results) == self.to_be_num:
+                if len(self.results["documents"]) == self.to_be_num:
                     break
         self.__get_articles()
-
-    def __get_articles(self):
-        """
-        :return: void
-         Makes calls to science direct to receive the article and abstracts based on the article eid number.
-        """
-        errors = 0
-        for i, doc in enumerate(tqdm(self.results)):
-            eid = doc["eid"]
-            eid_url = self.base_article_url+eid
-            eid_response = self.__exec_request(eid_url)
-            if eid_response is not "failed":
-                if isinstance(eid_response['full-text-retrieval-response']['originalText'], str):
-                    idx = find_nth(eid_response['full-text-retrieval-response']['originalText'], "1 Introduction", 2)
-                    idx = idx if idx != -1 else 0
-                    self.results[i]["article"] = eid_response['full-text-retrieval-response']['originalText'][idx:]
-                else:
-                    self.results[i]["article"] = False
-                self.results[i]["abstract"] = eid_response['full-text-retrieval-response']['coredata']['dc:description']
-            else:
-                self.results[i]["article"] = False
-                self.results[i]["abstract"] = False
-                errors += 1
-        print("{} Documents could not be found!".format(errors))
 
     def __exec_request(self, URL):
         """
@@ -102,6 +78,78 @@ class ScienceDirectCrawler:
         else:
             return "failed"
 
+    def __save_relevants_in_results(self, exec_result, total: bool = False):
+        """
+        :param exec_result: Dict (Server response of API request)
+        :param total: Boolean (Should the total result num be setted)
+        :return: void
+        This method stores the relevant information of the server response in the instance results variable
+        """
+        self.results["documents"] = list()
+        [self.results["documents"].append(dict()) for _ in exec_result['search-results']['entry']]
+        if total:
+            self.results["total_results"] = int(exec_result['search-results']['opensearch:totalResults'])
+        for i, doc in enumerate(exec_result['search-results']['entry']):
+            date_parts = self.__split_date(doc['prism:coverDate'][0]['$'])
+            self.results["documents"][i]["eid"] = doc['eid']
+            self.results["documents"][i]["title"] = doc["dc:title"]
+            self.results["documents"][i]["authors"] = self.__convert_authors(doc["authors"])
+            self.results["documents"][i]["date"] = doc['prism:coverDate'][0]['$']
+            self.results["documents"][i]["year"] = date_parts[0]
+            self.results["documents"][i]["month"] = date_parts[1]
+            self.results["documents"][i]["day"] = date_parts[2]
+
+    def __get_articles(self):
+        """
+        :return: void
+         Makes calls to science direct to receive the article and abstracts based on the article eid number.
+        """
+        errors = 0
+        for i, doc in enumerate(tqdm(self.results["documents"])):
+            eid = doc["eid"]
+            eid_url = self.base_article_url+eid
+            eid_response = self.__exec_request(eid_url)
+            if eid_response is not "failed":
+                if isinstance(eid_response['full-text-retrieval-response']['originalText'], str):
+                    idx = self.__get_introduction_index(eid_response['full-text-retrieval-response']['originalText'])
+                    self.results["documents"][i]["article"] = eid_response['full-text-retrieval-response']['originalText'][idx:]
+                else:
+                    self.results["documents"][i]["article"] = False
+                self.results["documents"][i]["abstract"] = eid_response['full-text-retrieval-response']['coredata']['dc:description']
+            else:
+                self.results["documents"][i]["article"] = False
+                self.results["documents"][i]["abstract"] = False
+                errors += 1
+        print("{} Documents could not be found!".format(errors))
+
+    def __get_introduction_index(self, text: str) -> int:
+        """
+        :param text: The text to be searched
+        :return: int
+        This method sould return the index of the SECOND occurrence of "1 Introduction" or "1. Introduction" or ...
+        """
+        values = ["1 Introduction", "1. Introduction", "1 INTRODUCTION", "1. INTRODUCTION"]
+        for v in values:
+            idx = find_nth(text, v, 2)
+            if idx != -1:
+                return idx
+        return 0
+
+    def __convert_authors(self, author_dict: dict):
+        """
+        :param author_dict: dict
+        :return: String of authors
+        Internal function to get the author names of an article
+        """
+        res = ""
+        for i, _ in enumerate(author_dict['author']):
+            res += author_dict['author'][i]["given-name"] + " " + author_dict['author'][i]["surname"] + ", "
+        return res
+
+    def __split_date(self, date: str):
+        parts = date.split("-")
+        return parts
+
     def __load_config(self):
         """
         :return: void
@@ -119,9 +167,9 @@ class ScienceDirectCrawler:
         """
         abs_errors = 0
         art_errors = 0
-        for doc in self.results:
-            file = open(self.out_dir + "[" + doc['prism:coverDate'][0]['$'] + "] " + doc["dc:title"] + ".txt", "w")
-            file.write(self.__convert_authors(doc["authors"]) + " " + doc["dc:title"] + "\n")
+        for doc in self.results["documents"]:
+            file = open(self.out_dir + "[{}] {}.txt".format(doc["date"], doc["title"]), "w")
+            file.write("{} - from Authors: {} \n".format(doc["title"], doc["authors"]))
             if doc["abstract"]:
                 file.write("\n\n______ABSTRACT______\n")
                 file.write(doc["abstract"])
@@ -134,35 +182,19 @@ class ScienceDirectCrawler:
                 art_errors += 1
         print("Not saved/found: {} abstracts, {} articles".format(abs_errors, art_errors))
 
-    def __convert_authors(self, author_dict: dict):
-        """
-        :param author_dict: dict
-        :return: String of authors
-        Internal function to get the author names of an article
-        """
-        res = ""
-        for i, _ in enumerate(author_dict['author']):
-            res += author_dict['author'][i]["given-name"] + " " + author_dict['author'][i]["surname"] + ", "
-        return res
-
     def save_to_dataframe(self):
         """
         :return: DataFrame
         Saves the results to a pandas dataframe
         """
         titles, years, months, days, authors = list(), list(), list(), list(), list()
-        for doc in self.results:
-            date_parts = self.__split_date(doc['prism:coverDate'][0]['$'])
-            titles.append(doc['dc:title'])
-            years.append(date_parts[0])
-            months.append(date_parts[1])
-            days.append(date_parts[2])
-            authors.append(self.__convert_authors(doc['authors']))
+        for doc in self.results["documents"]:
+            titles.append(doc['title'])
+            years.append(doc['year'])
+            months.append(doc['month'])
+            days.append(doc['day'])
+            authors.append(doc['authors'])
         return pd.DataFrame({"title": titles, "years": years, "months": months, "days": days, "author": authors})
-
-    def __split_date(self, date: str):
-        parts = date.split("-")
-        return parts
 
 
 if __name__ == "__main__":
@@ -170,4 +202,4 @@ if __name__ == "__main__":
     crawler.run()
     df = crawler.save_to_dataframe()
     print(df)
-    # crawler.save_to_file()
+    # rawler.save_to_file()
