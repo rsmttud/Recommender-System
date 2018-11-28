56ce04cce29ea0356eaebb49112e14770663886c
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-11-28 13:39:31 +0100

    Updated Crawler to clear issue with 400:Bad Request. Upper limit of 6k defined.

--- helper/data_obtaining/science_direct/ScienceDirectCrawler.py
+++ helper/data_obtaining/science_direct/ScienceDirectCrawler.py
@@ -4,18 +4,22 @@ import json
 import os
 from tqdm import tqdm
 from helper_functions import find_nth
+import re
+from rs_helper.classes.Crawler import Crawler
+import pickle
 
 
-class ScienceDirectCrawler:
+class ScienceDirectCrawler(Crawler):
 
-    def __init__(self, query: str, out_dir: str, to_be_num: int = 1000):
+    def __init__(self, query: str, out_dir: str, to_be_num: int = 1000, start: int = 0):
         """
         :param query: String (Query that should be searched in science direct (https://dev.elsevier.com/tips/ScienceDirectSearchTips.htm))
         :param out_dir: String (Directory where the resulting files should be stored)
         :param to_be_num: int (Amount of articles to get crawled (Needs to be x times 100))
         """
-        if to_be_num % 100 != 0:
-            raise ValueError("Number of results must be 100*x")
+        super().__init__(out_path=out_dir)
+        if to_be_num % 100 != 0 or to_be_num > 6000:
+            raise ValueError("Number of results must be 100*x and smaller than 6000")
         if not os.path.exists(out_dir):
             os.mkdir(out_dir)
         self.query = query
@@ -28,6 +32,7 @@ class ScienceDirectCrawler:
         self.config = self.__load_config()
         self.to_be_num = to_be_num
         self.results = dict()
+        self.results["documents"] = list()
         self.total_nums = 0
         self.num_res = 0
         self.out_dir = out_dir
@@ -39,22 +44,28 @@ class ScienceDirectCrawler:
         """
         result = self.__exec_request(self.url)
         self.__save_relevants_in_results(result, total=True)
-        self.num_res += len(self.results["documents"])
         self.total_nums = self.results["total_results"]
-
+        pbar = tqdm(total=self.total_nums / 100) if self.to_be_num > self.total_nums else tqdm(total=self.to_be_num/100)
+        pbar.update(1)
         if len(self.results["documents"]) != self.to_be_num:
             while self.num_res < self.total_nums:
+                # print("Is: {} | To be: {}".format(self.num_res, self.total_nums))
                 for el in result['search-results']['link']:
                     if el['@ref'] == 'next':
                         next_url = el['@href']
-                        next_response = self.__exec_request(next_url)
-                        self.__save_relevants_in_results(next_response)
-                        print(len(self.results["documents"]))
+                        result = self.__exec_request(next_url)
+                        self.__save_relevants_in_results(result)
+                        pbar.update(1)
                         if len(self.results["documents"]) == self.to_be_num:
                             break
                 if len(self.results["documents"]) == self.to_be_num:
                     break
-        self.__get_articles()
+        pbar.close()
+
+    def save_response(self, res):
+        file = open("response_{}.json".format(self.num_res), "w")
+        file.write(str(res))
+        file.close()
 
     def __exec_request(self, URL):
         """
@@ -74,7 +85,7 @@ class ScienceDirectCrawler:
         self._status_code = request.status_code
 
         if request.status_code == 200:
-            return json.loads(request.text)
+            return json.loads(request.text, strict=False)
         else:
             return "failed"
 
@@ -85,42 +96,46 @@ class ScienceDirectCrawler:
         :return: void
         This method stores the relevant information of the server response in the instance results variable
         """
-        self.results["documents"] = list()
-        [self.results["documents"].append(dict()) for _ in exec_result['search-results']['entry']]
+        current_idx = self.num_res
+        # print("Current index: {}".format(current_idx))
+        self.num_res += len(exec_result['search-results']['entry'])
+        # print("[Before saving in results dict] Number of current results: {}".format(self.num_res))
         if total:
             self.results["total_results"] = int(exec_result['search-results']['opensearch:totalResults'])
         for i, doc in enumerate(exec_result['search-results']['entry']):
             date_parts = self.__split_date(doc['prism:coverDate'][0]['$'])
-            self.results["documents"][i]["eid"] = doc['eid']
-            self.results["documents"][i]["title"] = doc["dc:title"]
-            self.results["documents"][i]["authors"] = self.__convert_authors(doc["authors"])
-            self.results["documents"][i]["date"] = doc['prism:coverDate'][0]['$']
-            self.results["documents"][i]["year"] = date_parts[0]
-            self.results["documents"][i]["month"] = date_parts[1]
-            self.results["documents"][i]["day"] = date_parts[2]
+            if "authors" in doc.keys():
+                authors = self.__convert_authors(doc["authors"])
+            else:
+                authors = ""
+            self.results["documents"].append(dict())
+            self.results["documents"][current_idx+i]["eid"] = doc['eid']
+            self.results["documents"][current_idx+i]["title"] = self.__prepare_title(doc["dc:title"])
+            self.results["documents"][current_idx+i]["authors"] = authors
+            self.results["documents"][current_idx+i]["date"] = doc['prism:coverDate'][0]['$']
+            self.results["documents"][current_idx+i]["year"] = date_parts[0]
+            self.results["documents"][current_idx+i]["month"] = date_parts[1]
+            self.results["documents"][current_idx+i]["day"] = date_parts[2]
 
-    def __get_articles(self):
+    def __get_article_and_abstract(self, eid: str):
         """
         :return: void
         Makes calls to science direct to receive the article and abstracts based on the article eid number.
         """
-        errors = 0
-        for i, doc in enumerate(tqdm(self.results["documents"])):
-            eid = doc["eid"]
-            eid_url = self.base_article_url+eid
-            eid_response = self.__exec_request(eid_url)
-            if eid_response is not "failed":
-                if isinstance(eid_response['full-text-retrieval-response']['originalText'], str):
-                    idx = self.__get_introduction_index(eid_response['full-text-retrieval-response']['originalText'])
-                    self.results["documents"][i]["article"] = eid_response['full-text-retrieval-response']['originalText'][idx:]
-                else:
-                    self.results["documents"][i]["article"] = False
-                self.results["documents"][i]["abstract"] = eid_response['full-text-retrieval-response']['coredata']['dc:description']
+        eid_url = self.base_article_url+eid
+        eid_response = self.__exec_request(eid_url)
+        text, abstr = False, False
+        if eid_response is not "failed":
+            if isinstance(eid_response['full-text-retrieval-response']['originalText'], str):
+                idx = self.__get_introduction_index(eid_response['full-text-retrieval-response']['originalText'])
+                text = eid_response['full-text-retrieval-response']['originalText'][idx:]
             else:
-                self.results["documents"][i]["article"] = False
-                self.results["documents"][i]["abstract"] = False
-                errors += 1
-        print("{} Documents could not be found!".format(errors))
+                text = False
+            if isinstance(eid_response['full-text-retrieval-response']['coredata']['dc:description'], str):
+                abstr = eid_response['full-text-retrieval-response']['coredata']['dc:description']
+            else:
+                abstr = False
+        return text, abstr
 
     def __get_introduction_index(self, text: str) -> int:
         """
@@ -141,9 +156,14 @@ class ScienceDirectCrawler:
         :return: String (The authors)
         Internal function to get the author names of an article
         """
+        if "author" not in author_dict.keys():
+            return str(author_dict)
         res = ""
         for i, _ in enumerate(author_dict['author']):
-            res += author_dict['author'][i]["given-name"] + " " + author_dict['author'][i]["surname"] + ", "
+            if "given-name" in author_dict["author"][i].keys() and "surname" in author_dict["author"][i].keys():
+                res += author_dict['author'][i]["given-name"] + " " + author_dict['author'][i]["surname"] + ", "
+            else:
+                res += str(author_dict['author'][i])
         return res
 
     def __split_date(self, date: str):
@@ -160,6 +180,9 @@ class ScienceDirectCrawler:
         file.close()
         return config_file
 
+    def __prepare_title(self, input):
+        return re.sub(r"[^A-Za-z| ]+", '', input)[:130]
+
     def save_to_file(self):
         """
         :return: void
@@ -167,17 +190,18 @@ class ScienceDirectCrawler:
         """
         abs_errors = 0
         art_errors = 0
-        for doc in self.results["documents"]:
-            file = open(self.out_dir + "[{}] {}.txt".format(doc["date"], doc["title"]), "w")
-            file.write("{} - from Authors: {} \n".format(doc["title"], doc["authors"]))
-            if doc["abstract"]:
-                file.write("\n\n______ABSTRACT______\n")
-                file.write(doc["abstract"])
+        for i, doc in enumerate(tqdm(self.results["documents"])):
+            paper, abstract = self.__get_article_and_abstract(doc["eid"])
+            if abstract:
+                abstract_file = open(self.out_dir + "[{}] Abstract_{}.txt".format(doc["date"], doc["title"]), "w")
+                abstract_file.write(abstract)
+                abstract_file.close()
             else:
                 abs_errors += 1
-            if doc["article"]:
-                file.write("\n\n______ARTICLE______\n")
-                file.write(doc["article"])
+            if paper:
+                file = open(self.out_dir + "[{}] {}.txt".format(doc["date"], doc["title"]), "w")
+                file.write(paper)
+                file.close()
             else:
                 art_errors += 1
         print("Not saved/found: {} abstracts, {} articles".format(abs_errors, art_errors))
@@ -198,8 +222,27 @@ class ScienceDirectCrawler:
 
 
 if __name__ == "__main__":
-    crawler = ScienceDirectCrawler(query="Title-Abstr-Key%28Clustering+AND+algorithm%29", to_be_num=100, out_dir="out/")
-    crawler.run()
-    df = crawler.save_to_dataframe()
-    print(df)
-    # rawler.save_to_file()
+    """
+    queries = ["Title-Abstr-Key%28Clustering+AND+data%29+AND+Title%28Clustering%29",
+               "Title-Abstr-Key%28Clustering+is%29+AND+Title%28Clustering%29",
+               "Title-Abstr-Key%28Classification+is%29+AND+Title%28Classification+OR+Prediction%29",
+               "Title-Abstr-Key%28Classification+AND+Prediction%29+AND+Title%28Classification+OR+Prediction%29", 
+               "Title-Abstr-Key%28Sequence+Analysis+is%29+AND+Title%28Sequence%29",
+               "Title-Abstr-Key%28Sequence+Analysis+AND+data%29+AND+Title%28Sequence%29",
+               "Title-Abstr-Key%28Sequence+Analytics+AND+data%29+AND+Title%28Sequence%29",
+               "Title-Abstr-Key%28Association+rule+AND+data%29+AND+Title%28Association%29",
+               "Title-Abstr-Key%28Association+rule+mining+is%29+AND+Title%28Association%29"]
+    """
+    queries = ["Title-Abstr-Key%28Sequential+Pattern%29",
+               "Title-Abstr-Key%28Frequent+Pattern%29"]
+    dirs = ["sequential pattern", "frequent pattern"]
+
+    for q, d in zip(queries, dirs):
+        print(q)
+        crawler = ScienceDirectCrawler(query=q,
+                                       to_be_num=6000,
+                                       out_dir="out/{}/".format(d))
+        crawler.run()
+        crawler.save_to_file()
+        df = crawler.save_to_dataframe()
+        crawler.pickle_dataframe(df, d)
