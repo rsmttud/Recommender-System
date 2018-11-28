9ee5465e1d6789d05ae2b0c5b2510640ded5dfe7
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-11-28 14:57:11 +0100

    Updated to ensure no duplicates are stored

--- helper_functions.py
+++ helper_functions.py
@@ -26,7 +26,33 @@ def save_txt_from_interface(title:str, short_desc:str, long_desc:str = None):
 
 
 def find_nth(string, substring, n):
+    """
+    :param string: String to search in
+    :param substring: String to find
+    :param n: int (Which occurrence)
+    :return: int (Index of the occurrence)
+    Function to find the n-th occurrence of a needle in a string. (Used by ScienceDirect Crawler)
+    """
     if n == 1:
         return string.find(substring)
     else:
         return string.find(substring, find_nth(string, substring, n - 1) + 1)
+
+
+def generate_dataset_of_files(files: list, class_name: str):
+    """
+    :param files: List(str) List of path files which should be merged to one single file in data/datasets/ directory
+    :param class_name: String (Class name and name of subdir in data/datasets/
+    :return: void
+    If directory of class does not exist it will be created
+    """
+    complete = []
+    for file in files:
+        content = open(file, "r").read()
+        if content not in complete:
+            complete.append(content)
+    if not os.path.exists("data/datasets/{}".format(class_name)):
+        os.mkdir("data/{}".format(class_name))
+    out_file = open("data/datasets/{}/{}.txt".format(class_name, class_name), "w")
+    out_file.write(" ".join(complete))
+    out_file.close()
