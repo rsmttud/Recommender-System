8df46634217bf12024ac0a961e3a288aa36f417a
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-11-28 13:35:20 +0100

    Updated Preprocessor methods. Need to be updated and discussed for fluent usage

--- rs_helper/classes/Preprocessor.py
+++ rs_helper/classes/Preprocessor.py
@@ -1,24 +1,26 @@
 import spacy
-import re
 
 
 class Preprocessor:
 
-    def __init__(self):
+    def __init__(self, data: str):
         self.nlp = spacy.load('en_core_web_sm')
+        self.doc = self.nlp(data)
 
-    def word_tokenize(self, sentence: str) -> list:
-        doc = self.nlp(sentence)
-        return [token.text.lower() for token in doc]
+    def word_tokenize(self) -> list:
+        return [token.lower_ for token in self.doc]
 
-    def sent_tokenize(self, document: str) -> list:
-        pass
+    def sent_tokenize(self) -> list:
+        return [sent for sent in self.doc.sents]
 
-    def remove_stopwords(self, tokens: list) -> list:
-        pass
+    def remove_stopwords(self) -> list:
+        return [token for token in self.doc if not token.is_stop]
 
-    def lemmatize(self, tokens: list) -> list:
-        pass
+    def lemmatize(self) -> list:
+        return [token.lemma_ for token in self.doc]
 
-    def remove_punct_and_nums(self, tokens: list) -> list:
-        pass
+    def remove_punct_and_nums(self) -> list:
+        return [token for token in self.doc if token.is_alpha and not token.is_punct]
+
+    def to_text(self) -> list:
+        return [el.lower_ for el in self.doc]
