6bc60e0e80b8dac5e6e0c851f432f876ad7cc22d
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-11-29 20:01:34 +0100

    Added the first version of the LDA Pipeline.

--- rs_helper/classes/RecommendationFacade.py
+++ rs_helper/classes/RecommendationFacade.py
@@ -1,7 +1,10 @@
 from rs_helper.classes import Corpora
 from rs_helper.classes import Vectorizer
 from rs_helper.classes import Prediction
-from rs_helper.classes import Preprocessor
+from rs_helper.classes.Preprocessor import Preprocessor
+import pickle
+import numpy as np
+
 
 
 class RecommendationFacade:
@@ -24,11 +27,19 @@ class RecommendationFacade:
             self.__key_ex_pipeline()
 
     def __lda_pipeline(self):
-        pass
+        model = pickle.load(open("models/LDA/LdaModel_ntopics_3_freq.bin", "rb"))
+        vectorizer = pickle.load(open("models/LDA/vectorizer.bin", "rb"))
+        preprocessor = Preprocessor(self.corpora.data)
+        docs = preprocessor.lemmatize(remove_stops=True, remove_punct_and_nums=True)
+        scores_per_sentence = list()
+        for doc in docs:
+            input_sentence = vectorizer.transform([" ".join(doc)])
+            topic_scores = model.transform(input_sentence)
+            scores_per_sentence.append(list(np.array(topic_scores).flatten()))
+        return scores_per_sentence
 
-    def __key_ex_pipeline(self):
-        pass
-
+    # TODO __merge_predictions() need to be implemented
+    """
     def __merge_predictions(self, predictions: list) -> Prediction:
         if not isinstance(predictions[0], Prediction):
             raise ValueError("Parameter predictions must be of type List(Prediction)")
