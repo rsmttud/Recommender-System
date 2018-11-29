36b6462d9e14a2bda170924720aa4edf93853840
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-11-29 20:01:54 +0100

    Small updated draft

--- rs_helper/classes/TranslationAugmentor.py
+++ rs_helper/classes/TranslationAugmentor.py
@@ -9,6 +9,7 @@ class TranslationAugmentator(Augmentator):
         :param sentences: List(String): list of sentences to translate
         :param languages: List(String): list of languages ISO-639-1 language codes.
         Class to create augmentations of sentences with Google Translate API
+        Langauge codes: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
         """
         super().__init__()
         self.translator = Translator()
@@ -26,20 +27,34 @@ class TranslationAugmentator(Augmentator):
         """
         for sent in self.data:
             self.augmented_data.append(sent)
-            translations = self.get_translations(sent)
+            translations = self.get_translations(sent, src="en")
             augmentations = self.get_back_translations(translations)
             for el in augmentations:
                 self.augmented_data.append(el)
         return self.augmented_data
 
-    def get_translations(self, sent: str):
+    def run_2(self):
+        augmentations = list()
+        for sent in self.data:
+            augmentations.append("ORIGINAL:" + sent)
+            augmentation = self.get_single_translation(sent)
+            augmentations.append(augmentation.text + "\n")
+        return augmentations
+
+    def get_single_translation(self, sent: str):
+        step_1_trans = self.translator.translate(sent, src="en", dest="kk")
+        step_2_trans = self.translator.translate(step_1_trans.text, src="kk", dest="gu")
+        return self.translator.translate(step_2_trans.text, src="gu", dest="en")
+
+    def get_translations(self, sent: str, src: str = "en"):
         """
         :param sent: String: sentence to translate
+        :param src: Source language key
         :return: Dict(String, String): Dict(Language code, Sentence)
         """
         translations = dict()
         for la in self.target_languages:
-            target = self.translator.translate(sent, src="en", dest=la)
+            target = self.translator.translate(sent, src=src, dest=la)
             translations[la] = target.text
         return translations
 
