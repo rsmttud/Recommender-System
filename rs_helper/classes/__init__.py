5e6641015f922dc3f06ac4f286977efa0c538ef1
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-12-02 11:41:27 +0100

    Added TFIDF and TFIGM

--- rs_helper/classes/__init__.py
+++ rs_helper/classes/__init__.py
@@ -4,9 +4,15 @@ from rs_helper.classes.RecommendationFacade import RecommendationFacade
 from rs_helper.classes.Prediction import Prediction
 from rs_helper.classes.SystemEvaluation import SystemEvaluation
 from rs_helper.classes.Preprocessor import Preprocessor
-#from rs_helper.classes.KeywordExtractor import KeywordExtractor
+from rs_helper.classes.KeywordExtractor import KeywordExtractor
 from rs_helper.classes.Topic import Topic
 from rs_helper.classes.Keyword import Keyword
+from rs_helper.classes.TranslationAugmentor import TranslationAugmentator
+from rs_helper.classes.Crawler import Crawler
+from rs_helper.classes.TFIDF import TFIDF
+from rs_helper.classes.TFIGM import TFIGM
+
+
 from rs_helper.classes.ArxivCrawler import ArxivCrawler
 #from rs_helper.classes.TranslationAugmentor import TranslationAugmentator
 from rs_helper.classes.EmbeddingClassificationPipeline import EmbeddingClassificationPipeline
