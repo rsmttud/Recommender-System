4a4b951114b3c4d6e2401f076e6ba95e29cbaa9c
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-12-02 11:41:13 +0100

    Added parameter to function __generate_topic

--- rs_helper/classes/KeywordExtractor.py
+++ rs_helper/classes/KeywordExtractor.py
@@ -7,18 +7,16 @@ from rs_helper.classes import Keyword
 class KeywordExtractor(ABC):
 
     def __init__(self):
-        self.class_name = self.__name__
+        self.class_name = self.__class__.__name__
 
-    @abstractmethod
-    def extract_keywords(self) -> list:
+    def extract_keywords(self, *kwargs) -> list:
         """
         Method to create find all keywords and their ranks
         :return: List(Keyword)
         """
         pass
 
-    @abstractmethod
-    def generate_topic(self) -> Topic:
+    def __generate_topic(self, token_rank_dict, label: str) -> Topic:
         """
         Method to transfer the keywords found into a Topic object.
         :return: Topic
