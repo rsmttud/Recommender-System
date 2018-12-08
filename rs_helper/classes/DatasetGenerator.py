36dd16817e6aa21d21ad92cb797322fbc536e043
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-12-08 11:49:05 +0100

    Updated and implemented merge of datasets

--- rs_helper/classes/DatasetGenerator.py
+++ rs_helper/classes/DatasetGenerator.py
@@ -6,29 +6,31 @@ import re
 import pickle
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+import shutil
 
 
 class DatasetGenerator:
 
-    def __init__(self, path_to_files: str, search_term: str, class_name: str, save_name: str):
+    def __init__(self, path_to_files: str, search_terms: list, class_name: str, save_name: str, seperate_save: bool = False):
         if not os.path.exists(path_to_files):
             raise ValueError("No such file or directory")
         self.path = path_to_files
         self.class_name = class_name
         self.result_sentences = list()
-        self.search_term = search_term
+        self.search_terms = search_terms
         self.save_name = save_name
+        self.seperate = seperate_save
 
     def run(self):
         sentences = self.find_sentences()
-        topics = pickle.load(open("manual_topics_for_embedding.pkl", "rb"))
+        topics = pickle.load(open("manual_topics_for_embedding_2.pkl", "rb"))
         all_words = self.get_corpora(topics, sentences)
-        clustering_topic = list(topics[self.class_name])
-        clustering_vector = self.vectorize(clustering_topic, all_words)
-        similarities = self.calculate_similarities(all_words, sentences, clustering_vector)
+        topic = list(topics[self.class_name])
+        topic_vector = self.vectorize(topic, all_words)
+        similarities = self.calculate_similarities(all_words, sentences, topic_vector)
         similarities = sorted(similarities.items(), key=lambda x: x[1], reverse=True)[:int(len(sentences)*0.25)]
         self.save_in_class(sentences, sim_list=similarities)
-        self.save_to_txt()
+        self.save_to_txt(seperate=self.seperate)
 
     def save_in_class(self, sentences: list, sim_list: list):
         for idx, sim in sim_list:
@@ -50,12 +52,12 @@ class DatasetGenerator:
         return vector
 
     def get_corpora(self, topics, sents):
+        print("Getting Copora:")
         all = list()
         for key in topics:
             for w in topics[key]:
                 if w not in all:
                     all.append(w)
-        print("Getting words of sentences:")
         for s in tqdm(sents):
             ws = word_tokenize(s)
             for w in ws:
@@ -69,10 +71,14 @@ class DatasetGenerator:
             if t.endswith(".txt"):
                 data = open(os.path.join(self.path, t), "r").read()
                 sentences = sent_tokenize(data)
+                # print("Sentences: {}".format(len(sentences)))
                 for s in sentences:
-                    s = re.sub('[^A-Za-z ]+', '', s)
-                    if s.lower().find(self.search_term) != -1:
-                        sents.append(s.lower())
+                    s = re.sub('[^A-Za-z ,.]+', '', s)
+                    s.replace("  ", " ")
+                    for needle in self.search_terms:
+                        if s.find("  ") == -1:
+                            if s.lower().find(needle) != -1 and s.lower() not in sents:
+                                sents.append(s.lower())
         return sents
 
     def save_to_dataframe(self):
@@ -80,17 +86,60 @@ class DatasetGenerator:
         df["class"] = self.class_name
         return df
 
-    def save_to_txt(self):
-        file = open(self.save_name + "_sentences.txt", "w")
-        for s in self.result_sentences:
-            file.write(s + "\n")
-        file.close()
+    def save_to_txt(self, seperate: bool = False):
+        if not seperate:
+            file = open("out/"+self.save_name + "_sentences.txt", "w")
+            for s in self.result_sentences:
+                file.write(s + "\n")
+            file.close()
+        else:
+            if not os.path.exists("out/"+self.class_name+"/"):
+                os.mkdir("out/"+self.class_name+"/")
+            for i, s in enumerate(self.result_sentences):
+                file = open("out/"+self.class_name+"/"+self.save_name+"_{}.txt".format(i), "w")
+                file.write(s)
+                file.close()
+
+    @staticmethod
+    def merge_crawl_results(super_dir: str):
+        merged_path = os.path.join(super_dir, "merged_datasets")
+        if not os.path.exists(merged_path):
+            os.mkdir(merged_path)
+        for sub_dir in tqdm(os.listdir(super_dir)):
+            label_dir = os.path.join(merged_path, sub_dir.split("-")[0])
+            if not os.path.exists(label_dir):
+                os.mkdir(label_dir)
+            sub_path = os.path.join(super_dir, sub_dir)
+            if not os.path.isdir(sub_path):
+                continue
+            for file in os.listdir(sub_path):
+                if file.endswith("txt"):
+                    if file not in os.listdir(label_dir):
+                        shutil.copyfile(os.path.join(sub_path, file), os.path.join(label_dir, file))
 
 
 if __name__ == "__main__":
-    generator = DatasetGenerator(path_to_files="../../data_obtaining/science_direct/out/sequence analysis",
-                                 search_term="sequence analysis is",
-                                 class_name="sequence_analysis",
-                                 save_name="sequence_analysis")
+    search_terms_for_patterns = ["frequent pattern mining is", "pattern analysis is", "frequent pattern mining aims",
+                                 "frequent pattern is", "frequent pattern mining defined",
+                                 "frequent patterns are", "pattern analysis", "sequential patterns are",
+                                 "sequential pattern mining is", "sequential pattern mining defined",
+                                 "pattern mining aims", "pattern mining is", "association rules are",
+                                 "association rule mining is", "association rule mining aims"]
+
+    search_terms_for_prediction = ["prediction aims", "prediction is", "classification is", "classification aims",
+                                   "classification defined", "classification defines", "regression is", "regression aims",
+                                   "regression defined", "regression defines", "regression analysis is", "classification analysis is",
+                                   "classification analysis aims", "regression analysis aims"]
+
+    search_terms_for_clustering = ["clustering is", "clustering defined", "clustering defines", "clustering aims",
+                                   "cluster analysis is", "cluster analysis aims", "cluster analysis defined",
+                                   "cluster analysis defines"]
+
+    generator = DatasetGenerator(path_to_files="../../data_obtaining/arxiv/out/frequent_pattern",
+                                 search_terms=search_terms_for_patterns,
+                                 class_name="sequential_pattern_mining",
+                                 save_name="frequent_pattern_mining_arxiv",
+                                 seperate_save=True)
     generator.run()
+    # generator.merge_crawl_results(super_dir="../../data_obtaining/science_direct/out/")
 
