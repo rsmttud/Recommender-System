d7a57e9f194cb71406900bd66e068a1af57000d5
MonteChristo46 <Ddad5100d!>
2018-12-10 12:25:30 +0100

    classification pipeline added

--- rs_helper/classes/RecommendationFacade.py
+++ rs_helper/classes/RecommendationFacade.py
@@ -2,12 +2,13 @@ from rs_helper.classes import Corpora
 from rs_helper.classes import Vectorizer
 from rs_helper.classes import Prediction
 from rs_helper.classes import Preprocessor
+from rs_helper.classes.EmbeddingClassificationPipeline import EmbeddingClassificationPipeline
 
 
 class RecommendationFacade:
 
     def __init__(self, path_to_file: str):
-        corpora = Corpora(path=path_to_file)
+        self.corpora = Corpora(path=path_to_file)
 
     def run(self, lda: bool = False, key_ex: bool = False, doc2vec: bool = False, classification: bool = False):
         """
@@ -22,9 +23,19 @@ class RecommendationFacade:
             self.__lda_pipeline()
         if key_ex:
             self.__key_ex_pipeline()
+        if classification:
+            return self.__classification_embedding()
 
     def __lda_pipeline(self):
         pass
+
+    def __classification_embedding(self):
+        path_classifier = "models/trained_models/daniel_0712/dnn_0712"
+        path_embedding = "models/trained_models/daniel_0712/USE_DAN/use_081218"
+        model = EmbeddingClassificationPipeline(path_to_embedding_model=path_embedding, path_to_model=path_classifier)
+        model.initialize()
+        prediction = model.predict(self.corpora.data)
+        return prediction
 
     def __key_ex_pipeline(self):
         pass
