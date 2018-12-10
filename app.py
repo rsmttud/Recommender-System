8a4559bbc411b2d896f3eaab7912823802b2cc3a
MonteChristo46 <Ddad5100d!>
2018-12-10 12:10:00 +0100

    Small changes.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
 from helper_functions import save_txt_from_interface
+from rs_helper import RecommendationFacade
+import os
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=["GET", "POST"])
 def main():
@@ -9,16 +12,29 @@ def main():
         problem_title = "" if "title" not in request.form else request.form['title']
         problem_short_desc = "" if "short_description" not in request.form else request.form['short_description']
         problem_long_desc = "" if "long_description" not in request.form else request.form['long_description']
+        problem_method = "" if "method" not in request.form else request.form['method']
 
-        save_txt_from_interface(problem_title, problem_short_desc, problem_long_desc)
+        file_name = save_txt_from_interface(problem_title, problem_short_desc, problem_long_desc)
+        prediction = []
 
-        return render_template("index.html", params={
+        if problem_method == "classification":
+            path = os.path.join("data/input/long_desc", file_name + ".txt")
+            facade = RecommendationFacade(path_to_file=path)
+            result = facade.run(classification=True)
+            prediction = zip(result.classes, result.values)
+
+        return render_template("index.html", prediction=prediction, params={
             "title": problem_title,
             "short": problem_short_desc,
-            "long": problem_long_desc
-        })
+            "long": problem_long_desc, })
 
-    return render_template("index.html")
+    else:
+        return render_template("index.html")
+
+
+@app.route("/process_main")
+def process_main():
+    return render_template("process_main.html")
 
 
 if __name__ == '__main__':
