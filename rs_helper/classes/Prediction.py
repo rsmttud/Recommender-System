a26314af9292ffce349ea35ce62e722cd9b1d053
MonteChristo46 <Ddad5100d!>
2018-12-10 12:25:04 +0100

    __repr__ method added

--- rs_helper/classes/Prediction.py
+++ rs_helper/classes/Prediction.py
@@ -1,21 +1,24 @@
-
+from typing import *
 
 class Prediction:
     """
     Class to store the final prediction of pipelines
     """
-    def __init__(self, classes: list, values: list):
+    def __init__(self, classes: List, values: List):
         """
         :param classes: List of all available classes (List(string))
         :param values: List of all values for all classes (List(float))
         """
-        if not isinstance(classes, list) or not isinstance(values, list):
+        if not isinstance(classes, List) or not isinstance(values, List):
             raise ValueError("Parameters classes and values need to be of type list")
         if len(classes) != len(values):
             raise ValueError("Lists classes and values need to have same length")
 
         self.classes = classes
         self.values = values
+
+    def __repr__(self):
+        return "{}: Classes: {}, Values: {}".format(type(self),self.classes, self.values)
 
     def get_class_with_max_confidence(self) -> str:
         """
