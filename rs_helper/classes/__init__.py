f7a758af5ee8107317e0d6cc9e3dac469b43ad4f
MonteChristo46 <Ddad5100d!>
2018-12-10 12:22:11 +0100

    New classes added

--- rs_helper/classes/__init__.py
+++ rs_helper/classes/__init__.py
@@ -4,7 +4,9 @@ from rs_helper.classes.RecommendationFacade import RecommendationFacade
 from rs_helper.classes.Prediction import Prediction
 from rs_helper.classes.SystemEvaluation import SystemEvaluation
 from rs_helper.classes.Preprocessor import Preprocessor
-from rs_helper.classes.KeywordExtractor import KeywordExtractor
+#from rs_helper.classes.KeywordExtractor import KeywordExtractor
 from rs_helper.classes.Topic import Topic
 from rs_helper.classes.Keyword import Keyword
-from rs_helper.classes.TranslationAugmentor import TranslationAugmentator
+from rs_helper.classes.ArxivCrawler import ArxivCrawler
+#from rs_helper.classes.TranslationAugmentor import TranslationAugmentator
+from rs_helper.classes.EmbeddingClassificationPipeline import EmbeddingClassificationPipeline
