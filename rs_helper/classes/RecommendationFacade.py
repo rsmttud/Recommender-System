ae483d2c698bc531c5f2a2d1315da32fb5520581
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-12-12 07:47:22 +0100

    Implementation of LDA Pipe

--- rs_helper/classes/RecommendationFacade.py
+++ rs_helper/classes/RecommendationFacade.py
@@ -1,10 +1,7 @@
 from rs_helper.classes import Corpora
-from rs_helper.classes import Vectorizer
 from rs_helper.classes import Prediction
-from rs_helper.classes import Preprocessor
 from rs_helper.classes.EmbeddingClassificationPipeline import EmbeddingClassificationPipeline
-import pickle
-import numpy as np
+from rs_helper.classes.LatentDirichletAllocation import LatentDirichletAllocation
 
 
 class RecommendationFacade:
@@ -22,25 +19,19 @@ class RecommendationFacade:
         :return: Object of type Prediction (merged prediction of all pipelines)
         """
         if lda:
-            self.__lda_pipeline()
+            return self.__lda_pipeline()
         if key_ex:
             self.__key_ex_pipeline()
         if classification:
             return self.__classification_embedding()
 
-    # TODO return Prediction Object
     def __lda_pipeline(self):
-        model = pickle.load(open("models/LDA/LdaModel_ntopics_3_freq_sd_arxiv.bin", "rb"))
-        vectorizer = pickle.load(open("models/LDA/vectorizer.bin", "rb"))
-        preprocessor = Preprocessor(self.corpora.data)
-        docs = preprocessor.lemmatize(remove_stops=True, remove_punct_and_nums=True)
-        scores_per_sentence = list()
-        for doc in docs:
-            input_sentence = vectorizer.transform([" ".join(doc)])
-            topic_scores = model.transform(input_sentence)
-            scores_per_sentence.append(list(np.array(topic_scores).flatten()))
-        return scores_per_sentence
-        pass
+        path_model = "models/LDA/LdaModel_ntopics_3_freq_sd_arxiv.bin"
+        path_vectorizer = "models/LDA/vectorizer.bin"
+        model = LatentDirichletAllocation(path_to_model=path_model, path_to_vectorizer=path_vectorizer)
+        model.initialize()
+        prediction = model.predict(self.corpora.data)
+        return prediction
 
     def __classification_embedding(self) -> Prediction:
         path_classifier = "./models/classifier/daniel_0712/dnn_0712"
