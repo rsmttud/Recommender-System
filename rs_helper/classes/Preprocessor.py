a4cefcc1ee39d641bdc6db593b970bc202289474
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-12-23 15:48:49 +0100

    Updated preprocessor to use CountVectorizer

--- rs_helper/classes/Preprocessor.py
+++ rs_helper/classes/Preprocessor.py
@@ -1,44 +1,48 @@
-import spacy
+from sklearn.feature_extraction.text import CountVectorizer
+import os
+import string
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
+
+
+class LemmaTokenizer(object):
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+
+    def __call__(self, text):
+        return [self.wnl.lemmatize(t) for t in word_tokenize(text)]
 
 
 class Preprocessor:
 
     def __init__(self, data: str):
-        self.nlp = spacy.load('en_core_web_sm')
-        self.doc = self.nlp(data)
+        self.data = [s for s in sent_tokenize(data)]
+        self.vectorizer = CountVectorizer(stop_words=list(stopwords.words("english")),
+                                          min_df=1, decode_error="ignore",
+                                          strip_accents="ascii", ngram_range=(1, 1),
+                                          tokenizer=LemmaTokenizer())
+        self.vectorizer.fit(self.data)
+        self.analyzer = self.vectorizer.build_analyzer()
 
-    def word_tokenize(self) -> list:
-        return [token.lower_ for token in self.doc]
+    def lemmatize(self):
+        self.data = [self.analyzer(s) for s in self.data]
 
-    def sent_tokenize(self) -> list:
-        return [sent for sent in self.doc.sents]
+    def remove_puctuation(self):
+        self.data = [[x for x in s if x not in string.punctuation] for s in self.data]
 
-    def remove_stopwords(self) -> list:
-        return [token for token in self.doc if not token.is_stop]
+    def remove_numbers(self):
+        self.data = [[x for x in s if re.match("[A-Za-z]+", x) is not None] for s in self.data]
 
-    def lemmatize(self, remove_stops: bool = True, remove_punct_and_nums: bool = True) -> list:
-        lemmatized = list()
-        for sent in self.doc.sents:
-            if not remove_punct_and_nums and not remove_stops:
-                lemmatized.append([token.lemma_ for token in sent])
-            if remove_stops and not remove_punct_and_nums:
-                lemmatized.append([token.lemma_ for token in sent if not token.is_stop])
-            if remove_punct_and_nums and not remove_stops:
-                lemmatized.append([token.lemma_ for token in sent if token.is_alpha and not token.is_punct])
-            if remove_stops and remove_punct_and_nums:
-                lemmatized.append([token.lemma_ for token in sent if token.is_alpha and not token.is_stop])
-        return lemmatized
-
-    def remove_double_spaces(self, strings):
-        for sent in strings:
-            sent.replace("  ", " ")
-            if sent.find("  ") != -1:
-                strings.remove(sent)
-        return strings
-
-    def remove_punct_and_nums(self) -> list:
-        return [token for token in self.doc if token.is_alpha and not token.is_punct]
-
-    def to_text(self) -> list:
-        return [el.lower_ for el in self.doc]
-
+    def transform(self, remove_nums: bool = True, remove_punct: bool = True):
+        self.lemmatize()
+        if remove_nums and not remove_punct:
+            self.remove_numbers()
+        if remove_punct and not remove_nums:
+            self.remove_puctuation()
+        if remove_nums and remove_punct:
+            self.remove_puctuation()
+            self.remove_numbers()
+        return self.data
