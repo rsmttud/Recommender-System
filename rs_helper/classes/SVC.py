62562006d7583b90534a3a6aa3ba33423fe2b634
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2018-12-23 15:49:45 +0100

    Initial commit of SVC model

---
+++ rs_helper/classes/SVC.py
@@ -0,0 +1,30 @@
+from rs_helper.classes.Model import Model
+from rs_helper.classes.Prediction import Prediction
+from rs_helper.classes.LabelMap import LabelMap
+import pickle
+import json
+
+
+class SVC(Model):
+
+    def __init__(self, path_to_model: str, path_to_vectorizer: str):
+        super().__init__(path_to_model=path_to_model)
+        self.vect = pickle.load(open(path_to_vectorizer, "rb"))
+
+    def initialize(self):
+        self.model = pickle.load(open(self.path, "rb"))
+
+    # TODO Talk about how to implement preprocessing
+    # TODO -> Using the TFIDFVectorizer of Sklearn with included preprocessing
+    def predict(self, text: str):
+        input = self.vect.transform([text])
+        resulting_class = self.model.predict(input)[0]
+        lm = LabelMap(path_to_json="models/label_maps/3_classes.json")
+        id = lm.get_index(resulting_class)
+        v = [0, 0, 0]
+        v[id] = 1
+        pred = Prediction(classes=[0, 1, 2], values=v)
+        return pred
+
+    def normalize_result(self, prediction: Prediction):
+        pass
