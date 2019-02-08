29b6326cd6d58cb2619a0acac5cf81f00b456f7c
MonteChristo46 <Ddad5100d!>
2019-02-08 13:59:48 +0100

    improvements

--- rs_helper/classes/EmbeddingModel.py
+++ rs_helper/classes/EmbeddingModel.py
@@ -3,6 +3,8 @@ from typing import *
 import tensorflow as tf
 import numpy as np
 import pickle
+from joblib import load
+
 
 
 class EmbeddingModel(ABC):
@@ -18,7 +20,6 @@ class EmbeddingModel(ABC):
         pass
 
 
-# TODO Implement Abstract Behavior
 class FastText(EmbeddingModel):
     """
     Loads the FastText model and get the Vectors.
@@ -27,13 +28,18 @@ class FastText(EmbeddingModel):
     def __init__(self, **kwargs):
         # super().__init__(**kwargs)
         super().__init__(**kwargs)
+        self.model = None
+        #self.path = "/Users/Daniel/PycharmProjects/Recommender-System/models/embeddings/FastText/FastText_model_20k_full.joblib"
         self.path = "/Users/Daniel/PycharmProjects/Recommender-System/notebooks/FastText/ft_model_15000.pkl"
         self.initialize_model()
 
     def initialize_model(self, **kwargs):
         try:
             tf.logging.info("FastText Model is loading")
-            self.model = pickle.load(open(self.path, "rb"))
+            if self.path.endswith(".pkl"):
+                self.model = pickle.load(open(self.path, "rb"))
+            else:
+                self.model = load(self.path)
             tf.logging.info("FastText Model loaded!")
         except Exception as e:
             tf.logging.warning("Something went wrong while loading the FastText Model..")
