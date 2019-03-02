7ef272b5b05d9dddf05865deb22ea52e209a683e
Richard Horn <richard.horn@mailbox.tu-dresden.de>
2019-03-02 10:48:40 +0100

    KW Extraction changes

--- rs_helper/classes/RecommendationFacade.py
+++ rs_helper/classes/RecommendationFacade.py
@@ -1,10 +1,11 @@
 from rs_helper.classes import Corpora
-from rs_helper.classes import Vectorizer
 from rs_helper.classes import Prediction
-from rs_helper.classes import Preprocessor
 from rs_helper.classes.EmbeddingClassificationPipeline import EmbeddingClassificationPipeline
+from rs_helper.classes.LatentDirichletAllocation import LatentDirichletAllocation
+from rs_helper.classes.KeywordExtractionPipeline import KeywordExtractionPipeline
+from rs_helper.classes.OneToOneGRU import OneToOneGRU
+from rs_helper.classes.SVC import SVC
 import pickle
-import numpy as np
 
 
 class RecommendationFacade:
@@ -12,46 +13,71 @@ class RecommendationFacade:
     def __init__(self, path_to_files: str):
         self.corpora = Corpora(path=path_to_files)
 
-    def run(self, lda: bool = False, key_ex: bool = False, doc2vec: bool = False, classification: bool = False):
+    def run(self, lda: bool = False, key_ex: bool = False,
+            doc2vec: bool = False, classification: bool = False,
+            svc_classification: bool = False, gru_oto: bool = False):
         """
         Function should manage the starting of pipelines according to input
         :param lda: boolean to start or not start LDA pipeline
         :param key_ex: boolean to start or not start Keyword Extraction pipeline
         :param doc2vec: boolean to start or not start Doc2Vec pipeline
         :param classification: boolean to start or not start ML Classification pipeline
+        :param svc_classification: boolean to start or not start ML SVC Classification pipeline
+        :param gru_oto: boolean to start the GRU One To One Classification pipeline
         :return: Object of type Prediction (merged prediction of all pipelines)
         """
         if lda:
-            self.__lda_pipeline()
+            return self.__lda_pipeline()
         if key_ex:
-            self.__key_ex_pipeline()
+            return self.__key_ex_pipeline()
         if classification:
             return self.__classification_embedding()
+        if svc_classification:
+            return self.__svc_classification()
+        if gru_oto:
+            return self.__gru_oto_classification()
 
-    # TODO return Prediction Object
     def __lda_pipeline(self):
-        model = pickle.load(open("models/LDA/LdaModel_ntopics_3_freq_sd_arxiv.bin", "rb"))
-        vectorizer = pickle.load(open("models/LDA/vectorizer.bin", "rb"))
-        preprocessor = Preprocessor(self.corpora.data)
-        docs = preprocessor.lemmatize(remove_stops=True, remove_punct_and_nums=True)
-        scores_per_sentence = list()
-        for doc in docs:
-            input_sentence = vectorizer.transform([" ".join(doc)])
-            topic_scores = model.transform(input_sentence)
-            scores_per_sentence.append(list(np.array(topic_scores).flatten()))
-        return scores_per_sentence
-        pass
+        path_model = "models/LDA/LdaModel_3_freq_clean.bin"
+        path_vectorizer = "models/LDA/vectorizer_3_freq_clean.bin"
+        model = LatentDirichletAllocation(path_to_model=path_model, path_to_vectorizer=path_vectorizer)
+        model.initialize()
+        prediction = model.predict(self.corpora.data)
+        return prediction
 
     def __classification_embedding(self) -> Prediction:
         path_classifier = "./models/classifier/daniel_0712/dnn_0712"
-        path_embedding = "models/trained_models/daniel_0712/USE_DAN/use_081218" # Still useless
+        path_embedding = "models/trained_models/daniel_0712/USE_DAN/use_081218"  # Still useless
         model = EmbeddingClassificationPipeline(path_to_embedding_model=path_embedding, path_to_model=path_classifier)
         model.initialize()
         prediction = model.predict(self.corpora.data)
         return prediction
 
+    def __svc_classification(self):
+        path_classifier = "models/classifier/SVC/SVC_tfidf_clean_.pkl"
+        path_vectorizer = "models/classifier/SVC/TFIDF_SVC_tfidf_clean_.pkl"
+        model = SVC(path_to_model=path_classifier, path_to_vectorizer=path_vectorizer)
+        model.initialize()
+        prediction = model.predict(self.corpora.data)
+        return prediction
+
     def __key_ex_pipeline(self):
-        pass
+        path_topics = "data/topics/"
+        model = KeywordExtractionPipeline(path_to_topics=path_topics)
+        model.initialize()
+        prediction = model.predict(self.corpora.data)
+        return prediction
+
+    def __gru_oto_classification(self):
+        path_model = "models/classifier/GRU_OtO/gru_one_to_one_equal_sets_200_units_0.2_dropout_20_epochs.yaml"
+        path_weights = "models/classifier/GRU_OtO/gru_one_to_one_equal_sets_weights_200_units_0.2_dropout_20_epochs.h5"
+        path_encoder = "notebooks/model_trainings/FastText/models/ft_model_15000.pkl"
+        model = OneToOneGRU(path_to_model=path_model,
+                            path_to_weights=path_weights,
+                            path_to_encoder=path_encoder)
+        model.initialize()
+        predictions = model.predict(self.corpora.data)
+        return predictions
 
     def __merge_predictions(self, predictions: list) -> Prediction:
         if not isinstance(predictions[0], Prediction):
---
+++ rs_helper/classes/SimilarityEval.py
@@ -0,0 +1,91 @@
+from rs_helper.classes import *
+from rs_helper.functions import *
+import json
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+import os
+
+
+class SimilarityEval:
+    def __init__(self, path, sim_data, valid_data):
+        """
+        :param path: path to FastText Model
+        :param sim_data: path to Similarity CSV
+        :param valid_data: path to validation data
+        Class to perform similarity evaluation
+        """
+        self.path = path
+        self.base_path = os.path.dirname(path)
+        self.folder = os.path.basename(os.path.dirname(path))
+        self.model = FastTextWrapper(path=path)
+        self.sim_data = sim_data
+        self.valid_data = valid_data
+        self.pearson_corr = None
+        self.spearman_corr = None
+        self.ME = None
+
+    def calculate_similarities(self):
+        """
+        :return: List(float): List of similarity values
+        Calculate similarities for word pairs in similiarity data
+        """
+        sims = list()
+        for i, r in self.sim_data.iterrows():
+            vecs = self.model.inference([r["Word 1"], r["Word 2"]])
+            if len(vecs) == 2:
+                sims.append(cosine_similarity([vecs[0]], [vecs[1]])[0][0])
+            else:
+                sims.append(np.nan)
+        self.sim_data["assigned_sim"] = sims
+        self.sim_data = self.sim_data.dropna()
+        self.mean_error()
+        self.correlation()
+        return sims
+
+    def mean_error(self):
+        """
+        :return: float
+        Calculate mean error of similarity judgements
+        """
+        self.ME = sum(np.array(self.sim_data["Human (mean)"]) - np.array(self.sim_data["assigned_sim"])) / len(
+            self.sim_data)
+        return self.ME
+
+    def correlation(self):
+        """
+        :return: List(float)
+        Correlation between human similarities and model similarities
+        """
+        self.pearson_corr = self.sim_data["Human (mean)"].corr(self.sim_data["assigned_sim"], method="pearson")
+        self.spearman_corr = self.sim_data["Human (mean)"].corr(self.sim_data["assigned_sim"], method="spearman")
+        return [self.pearson_corr, self.spearman_corr]
+
+    def save_to_config(self):
+        """
+        :return: None
+        Save similarity judgement values in config file
+        """
+        config_path = os.path.join(self.base_path, "config.json")
+
+        with open(config_path, "r") as _json:
+            c_dict = json.load(_json)
+
+        c_dict["mean_similarity_error"] = self.ME
+        c_dict["similarity_correlation"] = self.pearson_corr
+        c_dict["similarity_spearman_correlation"] = self.spearman_corr
+
+        with open(config_path, "w") as _json:
+            json.dump(c_dict, _json)
+
+    def plot_similarity(self):
+        """
+        :return: None
+        Create and save similarity matrix of valid data
+        """
+        self.valid_data["vector"] = self.valid_data["text"].apply(
+            lambda x: self.model.inference(word_tokenize(x), sentence_level=True))
+        messages = list(self.valid_data["label"])
+        vectors = list(self.valid_data["vector"])
+        similarity_matrix(messages=messages, vectors=vectors, name=self.folder, save_path=self.base_path)
