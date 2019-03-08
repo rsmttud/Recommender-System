836a96cacd6e8b24ed70bb12cfbbd8d78e145e27
MonteChristo46 <dhoeschele@gmx.net>
2019-03-08 23:02:04 +0100

    Swipe removed, D3 func added.

--- static/js/midleware.js
+++ static/js/midleware.js
@@ -1,68 +1,68 @@
-//TODO long desc, short, desc should be delivered as well
+
+/*TODO
+ * Cleanup Code (It's a mess),
+ * More Encapsulation in Function,
+ * Store long_desc, short_desc, classification_results for both, combined_classification result and final fore in json
+ * Save Json with timestamp (Can be used to retrain the model later in a semi-supervised way for example)
+ * Load Json for Donout Chart or other descriptive stuff
+ *
+  * */
 $("document").ready(function () {
-    $("#submit-button").click(function () {
+    $("#input-form").submit(function(e) {
+        e.preventDefault();
         $.ajax({
             data: {
-                short_description: $("#short_description").val(),
+                short_description: $('#short_description').val(),
                 long_description: $("#long_description").val(),
                 title: $("#title").val(),
                 method: $("#method").val()
             },
             type: "POST",
             url: "/classify",
-            beforeSend: function () {
-                console.log("tabs swiped");
+            beforeSend: function() {
+
                 //Swipe to next tab
                 $("#nav-input").removeClass("active");
+                $("#nav-input").parent("li").addClass("disabled");
                 $("#nav-process").addClass("active");
-                $("#tabs-swipe-demo").tabs({swipeable: true});
+                $("#nav-process").parent("li").removeClass("disabled");
+                $("#tabs-swipe-demo").tabs({swipeable: false});
             },
             success: function (data) {
-                // Swipe to next tab
-                $("#tabs-swipe-demo").tabs({swipeable: false});
-                $("#nav-input").removeClass("active");
-                $("#nav-process").removeClass("active");
-                $("#nav-result").addClass("active");
-                $("#tabs-swipe-demo").tabs({swipeable: true});
-                console.log("tabs swiped again k.");
-                //$("#tabs-swipe-demo").tabs({swipeable: true});
+                $("#nav-input").parent("li").removeClass("disabled");
 
-                //Remove all UL under #output-result-main
-                $("#output-result-main > ul").remove();
+                $("#nav-process").removeClass("active");
+                $("#nav-process").parent("li").addClass("disabled");
+                $("#nav-result").addClass("active");
+                $("#nav-result").parent("li").removeClass("disabled");
+
+                $("#nav-download").parent("li").removeClass("disabled");
+                $("#tabs-swipe-demo").tabs({swipeable: false});
+
+                //Remove html tags before writing new information to them
+                $("#output-result-main > h5").remove();
+                $("#donut-chart > svg").remove();
+
+
+                // Get key with highest value from dict
+                var most_prob_class = Object.keys(data).reduce(function(a, b){ return data[a] > data[b] ? a : b });
 
                 // Writing input in result page
-                let ul = $("<ul/>").appendTo($("#output-result-main"));
+                $("#output-result-main-class")
+                    .append("<h6>"+most_prob_class);
+
+                // Preparing the data for donout_chart.js
+                var lst = [];
                 $.each(data, function(key, value){
-                    $('<li/>')
-                        .attr('role', 'menuitem')
-                        .text(key + ": " + value)
-                        .appendTo(ul)
+                    lst.push({"class": key, "prob": value})
                 });
+
+                var _json = {forecast: lst};
+                //Path where json is stored..
+                donoutChart(_json);
+                console.log(data)
             }
         })
+
     });
 });
-
-//Returns String
-function createOutputJSON(long_desc, short_desc, dict_result_long, dict_result_short){
-    //Transforming to JSON
-    /*
-    {
-        short_desc: {
-            message:
-            classes:
-            probabilities:
-        }
-        long_desc: {
-            message:
-            classes:
-            probabilities:
-        }
-    }
-
-     */
-}
-//Save File
-function saveJSONString(file_name, json_string){
-    //Save incoming JSON String to file
-}
