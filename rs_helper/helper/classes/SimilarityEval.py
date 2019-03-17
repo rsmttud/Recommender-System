d268b8937baf017f6d12c5639de03972c593102e
MonteChristo46 <dhoeschele@gmx.net>
2019-03-17 15:07:27 +0100

    Typing corrected

--- rs_helper/helper/classes/SimilarityEval.py
+++ rs_helper/helper/classes/SimilarityEval.py
@@ -5,6 +5,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from nltk.tokenize import word_tokenize
 from rs_helper.core import *
 from rs_helper.helper import *
+from typing import List
 
 
 class SimilarityEval:
@@ -60,7 +61,7 @@ class SimilarityEval:
             self.sim_data)
         return self.ME
 
-    def correlation(self) -> List[float, float]:
+    def correlation(self) -> List[float]:
         """
         Correlation between human similarities and model similarities
         Spearman and Pearson correlation are calculated.
