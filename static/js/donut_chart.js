c32de765a4a064a4841fb69d6fba3d3633e0d170
MonteChristo46 <dhoeschele@gmx.net>
2019-03-19 21:59:47 +0100

    legend and mouseover added

--- static/js/donut_chart.js
+++ static/js/donut_chart.js
@@ -1,17 +1,17 @@
 function donutChart(data) {
     data = data["forecast"]; //Place in dict where final prediction values are stored
-    console.log(data);
+
     var margin = {top: 20, right: 20, bottom: 20, left: 20},
-        width = 400 - margin.right - margin.left,
+        width = $("#donut-chart").width() - margin.right - margin.left,
         height = 400 - margin.top - margin.bottom,
-        radius = width / 2.5;
+        radius = width / 3.0;
 
     var color = d3.scaleOrdinal()
         .range(["#b4126b", "#007581", "#055E67", "#C0AD26"]); //Color range
     // Arc Generator produces the arcs for the pie generator
     var arc = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(radius - 50);
+        .outerRadius(radius - 50)
+        .innerRadius(radius - 100);
 
     var labelArc = d3.arc()
         .outerRadius(radius)
@@ -30,24 +30,61 @@ function donutChart(data) {
         .attr("width", width)
         .attr("height", height)
         .append("g")
-        .attr("transform", "translate(" + width / 2 + ", " + height / 2 + ")"); //Set the group object to the middlepoint
+        .attr("transform", "translate(" + width / 2.8 + ", " + height / 2 + ")"); //Set the group object to the middlepoint
 
     //Appending g elements and arcs
     var g = svg.selectAll(".arc")
         .data(pie(data))
         .enter()
         .append("g")
-        .attr("class", "arc");
+        .attr("class", "arc")
+        .attr("id", function (d) {
+            return d.data.class
+        });
 
+    //Legend
+    var legend = svg.selectAll(".legend")
+        .data(pie(data))
+        .enter()
+        .append("g")
+        .attr("class", "legend")
+        .style("cursor", "pointer")
+        .attr("legend-id", function (d) {
+            return d.data.class
+        })
+        .attr("transform", function (d, i) {
+            return "translate(30," + (-70 * i) + ")"
+        });
+    // Legend Entry
+    var leg = legend.append("rect");
+
+    //Tooltip for arcs
     var tooltip = svg.append("g")
         .attr("class", "tooltip")
-        .attr("display", "none")
         .style("cursor", "pointer");
 
+    //Build Legend
+    //Change x if you want to have the rectangles on the other side
+    leg.attr("x", width / 2)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", function (d) {
+            return color(d.data.class)
+        });
+
+    legend.append("text")
+        .attr("x", (width / 2) - 5)
+        .attr("dy", ".8em")
+        .style("text-anchor", "end")
+        .text(function (d) {
+            return d.data.class
+        });
+
+    //Build Tooltip
     tooltip.append("text")
         .attr("x", 15)
         .attr("dy", "1.2em")
-        .style("font-size", "1.25em");
+        .style("font-size", "1.6em");
 
     //Append path of the arc == Curve of the arc
     g.append("path")
@@ -60,19 +97,17 @@ function donutChart(data) {
         .duration(1500)
         .attrTween("d", transitionPie);
 
-    //append labels
-    /*
-    g.append("text")
-        .attr("transform", function (d) {
-            return "translate(" + labelArc.centroid(d) + ")"
-        })
-        .attr("dy", "0px")
-        .attr("dx", "0px")
-        .text(function (d) {
-            return "" + d.data.prob + "%"
-        });
-     */
+    // Mouseover effects
+    //Legend
+    legend.on("mouseover", function (d) {
+        $("#" + d.data.class).find("path").css("transform", "scale(1.15)");
 
+    })
+        .on("mouseout", function (d) {
+            $("#" + d.data.class).find("path").css("transform", "scale(1.0)");
+        });
+
+    //Tooltip
     g.on("mouseover", function () {
             tooltip.style("display", "block")
         }
@@ -84,9 +119,9 @@ function donutChart(data) {
             var xPos = d3.mouse(this)[0] - 15;
             var yPos = d3.mouse(this)[1] - 15;
             tooltip.attr("transform", "translate(" + xPos + ", " + yPos + ")");
-            tooltip.select("text").text("class: "+d.data.class+" " +
-                "prob: "+d.data.prob)
+            tooltip.select("text").text(d.data.prob + "%")
         });
+
 
     function transitionPie(_arc) {
         _arc.innerRadius = 0;
