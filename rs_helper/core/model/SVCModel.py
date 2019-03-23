79a686a4e6c0fb1acfb9202592fe74dd79464371
MonteChristo46 <dhoeschele@gmx.net>
2019-03-23 21:36:33 +0100

    rs_helper type hinting update

--- rs_helper/core/model/SVCModel.py
+++ rs_helper/core/model/SVCModel.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.svm import SVC
 import os
 from rs_helper.core.model.Model import Model
 from rs_helper.core.Prediction import Prediction
@@ -13,15 +14,20 @@ from typing import List
 
 class SVCModel(Model):
 
-    def __init__(self, path_to_model: str, embedding_model: EmbeddingModel, label_map = "models/label_maps/3_classes.json"):
+    def __init__(self, path_to_model: str,
+                 embedding_model: EmbeddingModel):
+
         super().__init__(path_to_model)
         self.model = None
         self.embedding_model = embedding_model
-        self.label_map = LabelMap(label_map)
+        self.label_map = None
         self.initialize()
 
     def initialize(self) -> None:
         self.model = load(self.path)
+        self.label_map = LabelMap(path_to_json=os.path.join(os.path.dirname(self.path), "label_map.json"))
+        if not isinstance(self.model, SVC):
+            raise ValueError("Supplied model not of type sklearn.svm.SVC")
 
     def predict(self, text: str) -> Prediction:
         if isinstance(self.embedding_model, FastTextWrapper):
@@ -30,15 +36,11 @@ class SVCModel(Model):
             embeddings = self.embedding_model.inference(word_tokenize(text))
 
         probs = self.model.predict_proba(embeddings)
-        lm = LabelMap(path_to_json=os.path.join(os.path.dirname(self.path), "label_map.json"))
         classes = []
         values = []
 
         for x, y in enumerate(probs[0]):
-            classes.append(lm.get_name(x))
+            classes.append(self.label_map.get_name(x))
             values.append(y)
 
         return Prediction(classes, values)
-
-    def normalize_result(self, prediction: Prediction) -> Prediction:
-        pass
